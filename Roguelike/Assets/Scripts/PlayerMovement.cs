21409a894fed954eb80919230199640af7a2826c
jorroz <jortvandoorn@gmail.com>
2019-10-10 09:23:55 +0200

    weewoo

--- Roguelike/Assets/Scripts/PlayerMovement.cs
+++ Roguelike/Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,11 @@ public class PlayerMovement : MonoBehaviour
 
     // Reference to Rigidbody
     public Rigidbody2D rb;
+    public Camera cam;
 
     // Movement Vector2; used for detecting player input (x, y)
     private Vector2 movement;
+    Vector2 mousePos;
 
     // Function that gets called upon start
     private void Start()
@@ -25,6 +27,8 @@ public class PlayerMovement : MonoBehaviour
         // Gets a value of 1 (right) or -1 (left) based on player input; See: InputManager
         movement.x = Input.GetAxisRaw("Horizontal");  // Input on Horizontal Axis
         movement.y = Input.GetAxisRaw("Vertical");  // Input on Vertical Axis
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     // Same as Update() but for application of the PhysicsEngine
@@ -32,5 +36,9 @@ public class PlayerMovement : MonoBehaviour
     {
         // Adding movement to the current position and multiplying it by the movespeed
         rb.MovePosition(rb.position + movement * MoveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
