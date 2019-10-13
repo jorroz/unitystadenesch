cb31ee585e3be1ac4d7d1c32efeba0914c76bce7
jorroz <jortvandoorn@gmail.com>
2019-10-13 12:37:48 +0200

    Fatma's update

--- Roguelike/Assets/Scripts/EnemyController.cs
+++ Roguelike/Assets/Scripts/EnemyController.cs
@@ -5,6 +5,14 @@ using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour
 {
+    public float speed;    public float stoppingDistance;    public float retreatDistance;
+
+    public float timeBtwShots;
+    public float startTimeBtwShots;
+
+
+    public GameObject projectile;
+    public Transform Player;   
 
     //public int startHealth = 100;
     public float health = 100f;
@@ -15,12 +23,36 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent <Transform>();
+        timeBtwShots = startTimeBtwShots;
+
         //health = startHealth;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+    {   if (Vector2.Distance(transform.position, Player.position) > stoppingDistance)        {            transform.position = Vector2.MoveTowards(transform.position, Player.position, speed * Time.deltaTime);        }
+        else if (Vector2.Distance(transform.position, Player.position) < stoppingDistance && Vector2.Distance(transform.position, Player.position) > retreatDistance)
+        {            transform.position = this.transform.position;        }
+        else if (Vector2.Distance(transform.position, Player.position) < retreatDistance)
+        {            transform.position = Vector2.MoveTowards(transform.position, Player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBtwShots <=0){
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+
+
+        }else {
+            timeBtwShots -= Time.deltaTime;
+        }
+        
+
+
+
+
         Debug.Log(health);
 
         healthBar.fillAmount = health / 100f;
