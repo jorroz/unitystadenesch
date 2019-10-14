986ef7aaeb070426f6c86cdc7d74176cc3603255
jorroz <jortvandoorn@gmail.com>
2019-10-14 09:34:51 +0200

    EnemyBullet aanpassingen

--- Roguelike/Assets/Scripts/EnemyBullet.cs
+++ Roguelike/Assets/Scripts/EnemyBullet.cs
@@ -5,8 +5,31 @@ using UnityEngine;
 public class EnemyBullet : MonoBehaviour
 {
 
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+
+
     public GameObject hitEffect;
 
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
+        
+           
+        
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -25,4 +48,10 @@ public class EnemyBullet : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+
+    void DestroyProjectile()
+    {
+        Destroy(this.gameObject);
+    }
 }
+
