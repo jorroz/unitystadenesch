7e9a8b6e8b304196b69f9d1c8ac54506bffa06d5
RedGalaxySoftware <onankman@gmail.com>
2019-10-14 13:21:20 +0200

    Level generator now generates doors

--- Roguelike/Assets/RoomGen/RoomDefinition.cs
+++ Roguelike/Assets/RoomGen/RoomDefinition.cs
@@ -1,33 +1,41 @@
 using System;
 
-namespace RoomGen {
-    public class RoomDefinition {
+namespace RoomGen
+{
+    public class RoomDefinition
+    {
         public int minWidth, maxWidth, minHeight, maxHeight;
         private Room room;
 
-        public Room Room {
-            get {
+        public Room Room
+        {
+            get
+            {
                 return room;
             }
         }
 
-        internal void SetRoom( Room room ) {
+        internal void SetRoom(Room room)
+        {
             this.room = room;
         }
 
-        public RoomDefinition( int minWidth, int maxWidth, int minHeight, int maxHeight ) {
+        public RoomDefinition(int minWidth, int maxWidth, int minHeight, int maxHeight)
+        {
             this.minWidth = minWidth;
             this.maxWidth = maxWidth;
             this.minHeight = minHeight;
             this.maxHeight = maxHeight;
         }
 
-        public int GetRandomWidth( Random rand ) {
-            return rand.Next( minWidth, maxWidth );
+        public int GetRandomWidth(Random rand)
+        {
+            return rand.Next(minWidth, maxWidth);
         }
 
-        public int GetRandomHeight( Random rand ) {
-            return rand.Next( minHeight, maxHeight );
+        public int GetRandomHeight(Random rand)
+        {
+            return rand.Next(minHeight, maxHeight);
         }
     }
 }
